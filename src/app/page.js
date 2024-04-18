722abc3d7c23f21f567e3ebb59659ae989aa5c04
surajkabirath <surajkabirath1234@gmail.com>
2024-04-19 01:32:07 +0545

    home page updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,26 @@
-import Image from "next/image";
+import Results from "@/components/Results";
 
-export default function Home() {
-  return (
-<></>
+const API_KEY = process.env.NEXT_APP_API_KEY;
+const Home = async ({ searchParams }) => {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
-}
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
+};
+
+export default Home;
