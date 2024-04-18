a6930f942d20417dbad0ee457e4327a3d7d4f0fa
surajkabirath <surajkabirath1234@gmail.com>
2024-04-19 01:06:21 +0545

    navbar update

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,6 +1,7 @@
+"use client"
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+;
 
 const NavbarItem = ({ title, param }) => {
   const searchParams = useSearchParams();
