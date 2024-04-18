2a82903978af219c358ead604f40487ded7eb229
surajkabirath <surajkabirath1234@gmail.com>
2024-04-18 21:40:09 +0545

    app page updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
+import Navbar from "@/component/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      <h1>My Homepage</h1>
-      <p>Welcome to my homepage!</p>
+     <Navbar/>
+      <h1>Home</h1>
     </div>
   )
 }
