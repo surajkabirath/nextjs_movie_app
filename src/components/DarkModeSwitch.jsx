29fd15d4ff7d2947f70cb0bdaff408cb5611f53a
surajkabirath <surajkabirath1234@gmail.com>
2024-04-19 00:36:43 +0545

    fix the problem in darkTheme Mode

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,23 +1,27 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
 const DarkModeSwitch = () => {
   const { theme, setTheme, systemTheme } = useTheme();
+  const [mode, setMode] = useState(false);
   const currentTheme = theme === "system" ? systemTheme : theme;
+  useEffect(() => setMode(true), []);
   return (
     <div>
-      {currentTheme === "dark" ? (
-        <MdLightMode
-          onClick={() => setTheme("light")}
-          className="text-2xl cursor-pointer"
-        />
-      ) : (
-        <MdDarkMode
-          onClick={() => setTheme("dark")}
-          className="text-2xl cursor-pointer"
-        />
-      )}
+      {mode &&
+        (currentTheme === "dark" ? (
+          <MdLightMode
+            onClick={() => setTheme("light")}
+            className="text-2xl cursor-pointer"
+          />
+        ) : (
+          <MdDarkMode
+            onClick={() => setTheme("dark")}
+            className="text-2xl cursor-pointer"
+          />
+        ))}
     </div>
   );
 };
