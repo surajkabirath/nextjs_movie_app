a38ee813ac93bf56ebfa78c4a24e22fdd9222b9f
surajkabirath <surajkabirath1234@gmail.com>
2024-04-19 02:14:51 +0545

    loading effect add

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const loading = () => {
   return (
-    <div>loading</div>
+    <div className='flex justify-center mt-16'>
+      <Image className='h-52'  src="spinner.svg" alt='loading...' />
+    </div>
   )
 }
 
