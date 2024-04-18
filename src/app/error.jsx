50c5ab27f9f52353360e471f8ef0aedb9ca287a4
surajkabirath <surajkabirath1234@gmail.com>
2024-04-19 01:59:29 +0545

    error handle

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,8 +1,17 @@
-"use client"
-const error = () => {
-  return (
-    <div>error</div>
-  )
-}
+"use client";
 
-export default error
+import { useEffect } from "react";
+
+const Error = ({ error, reset }) => {
+  useEffect(() => {
+    error.message && console.log(error.message);
+  }, [error]);
+  return (
+    <div className='text-center mt-10'>
+      <h1>Something went wrong , Please try again later.</h1>
+      <button className='hover:text-amber-600' onClick={reset}>Try Again</button>
+    </div>
+  );
+};
+
+export default Error;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { FiThumbsUp } from "react-icons/fi";
 
-const Card = () => {
+const Card = ({ result }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
+      <Link href={`/movie/${result.id}`}>
+       
+          <Image
+            layout="responsive"
+            src={`https://image.tmdb.org/t/p/original${result.backdrop_path || result.poster_path}`}
+            height={300}
+            width={500}
+            className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
+            alt="poster"
+          ></Image>
+          <div className="p-2">
+            <h2 className="truncate text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-semibold text-slate-400 group-hover:text-slate-500">
+              {result.title || result.name}
+            </h2>
+            <p className="truncate text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl text-slate-400 group-hover:text-slate-500">
+              {result.overview}
+            </p>
+            <p className="flex items-center text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl text-slate-400 group-hover:text-slate-500">
+              <FiThumbsUp className="mr-1" />
+              {result.vote_count}
+            </p>
+          </div>
+     
 
-export default Card
+      </Link>
+    
+   
+    </div>
+  );
+};
+
+export default Card;
