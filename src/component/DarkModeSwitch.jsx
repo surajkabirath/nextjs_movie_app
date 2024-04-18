9612ef312538ff9244e75eed94d0680df88c8d44
surajkabirath <surajkabirath1234@gmail.com>
2024-04-18 21:17:38 +0545

    component file added

---
+++ src/component/DarkModeSwitch.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DarkModeSwitch = () => {
+  return (
+    <div>DarkModeSwitch</div>
+  )
+}
+
+export default DarkModeSwitch
---
+++ src/component/NavbarItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavbarItem = () => {
+  return (
+    <div>NavbarItem</div>
+  )
+}
+
+export default NavbarItem
