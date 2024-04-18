6a0a44a3ca16814d0b4c2607601dcc2caba0230f
surajkabirath <surajkabirath1234@gmail.com>
2024-04-19 00:56:33 +0545

    add navbar with navbarItem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client"
+import React from "react";
+import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-lg justify-center gap-6">
+      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Top Rated" param="fetchTopRated" />
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
-const NavbarItem = () => {
+const NavbarItem = ({ title, param }) => {
+  const searchParams = useSearchParams();
+  const currentParam = searchParams.get("genre");
   return (
-    <div>NavbarItem</div>
-  )
-}
+    <div>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          currentParam === param
+            ? "'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'"
+            : ""
+        } cursor-pointer`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+};
 
-export default NavbarItem
+export default NavbarItem;
