5cab8d12d6389e630fc860c93dbc7fc01996c8c9
surajkabirath <surajkabirath1234@gmail.com>
2024-04-30 09:10:01 +0545

    final

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,10 +1,9 @@
 import Link from 'next/link'
 
 
-const MenuItem = ({title, address, Icon}) => {
+const MenuItem = ({title, address}) => {
   return (
     <Link href={address} className='hover:text-amber-500'>
-      <Icon className="text-2xl sm:hidden"/>
       <p className='uppercase hidden sm:inline text-sm'>{title}</p>
     </Link>
   )
